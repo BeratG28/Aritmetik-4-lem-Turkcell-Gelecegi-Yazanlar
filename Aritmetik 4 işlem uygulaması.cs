243808b31cd88943e7dc3ccead5fd368ffe3fe25
Berat Günaydın <116026350+BeratG28@users.noreply.github.com>
2022-11-30 14:05:57 +0300

    Add files via upload

---
+++ Aritmetik 4 işlem uygulaması.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1, s2, toplam,carpım, bolum, fark ;
+
+            Console.WriteLine("***** Aritmetik İşlemler *****");
+            
+            Console.WriteLine();
+            s1 = 20;
+            s2 = 5;
+            toplam = s1 + s2;
+            fark = s1 - s2;
+            carpım = s1 * s2;
+            bolum = s1 / s2;
+
+            Console.WriteLine("Toplam: " + toplam);
+            Console.WriteLine("Fark: " + fark);
+            Console.WriteLine("Carpım: " + carpım);
+            Console.WriteLine("Bolum: " + bolum);
+            
+            Console.WriteLine();
+
+            
+            Console.WriteLine("***** Aritmetik İşlemler *****");
+
+            Console.Read();
+
+
+        }
+    }
+}
